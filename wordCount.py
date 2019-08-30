407fb911adb529bcc3acf1886af790d8b0618784
Jonathan Roman <jonathan.roman519@gmail.com>
2019-08-29 23:46:12 -0600

    Added wordCounter method to take a list of words and return a dictionary with the words as keys and the occurences as the value

--- wordCount.py
+++ wordCount.py
@@ -1,5 +1,5 @@
 import string 
-#Reads file and places each word as part of a list without punctiation, in alphabetical order, and in lowercase
+#Reads file, removes punctuation and returns a list of words in alphabetical order, and in lowercase
 def fileReader(file):
 	text = open(file,"r")
 	words = text.read()
@@ -8,10 +8,21 @@ def fileReader(file):
 	seperatedWords = words.split()
 	seperatedWords = [element.lower() for element in seperatedWords]
 	seperatedWords.sort()
-	for x in seperatedWords:
-		print(x)
 	text.close()
+	return seperatedWords
+
+#Takes a list of words and places them in a dictionary with the word as a key and the occurrences as its value
+def wordCounter(words):
+	wordCount = {}
+	for x in words:
+		if(x in wordCount):
+			wordCount[x] = wordCount[x] + 1
+		else:
+			wordCount[x] = 1
+	print(wordCount)
+
 
 #main method
 if __name__=="__main__":
-    fileReader("speech.txt")
+    organizedWords=fileReader("speech.txt")
+    wordCounter(organizedWords)
