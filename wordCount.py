ab6964ffe0b54a023987937359414a137998bb0a
Jonathan Roman <jonathan.roman519@gmail.com>
2019-08-29 23:08:26 -0600

    fileReader now reads a file and prints the list of words in alphabetical order and all words are changed to lowecase

--- wordCount.py
+++ wordCount.py
@@ -1,11 +1,13 @@
 import string 
-#Reads file and places each word as part of a list
+#Reads file and places each word as part of a list without punctiation, in alphabetical order, and in lowercase
 def fileReader(file):
 	text = open(file,"r")
 	words = text.read()
 	table = words.maketrans("!;',.?:\"-","         ")
 	words = words.translate(table)
 	seperatedWords = words.split()
+	seperatedWords = [element.lower() for element in seperatedWords]
+	seperatedWords.sort()
 	for x in seperatedWords:
 		print(x)
 	text.close()
