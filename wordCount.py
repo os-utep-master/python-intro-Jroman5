d58eeec5fdb7262af0fdb31a37c450dff5bebca8
Jonathan Roman <jonathan.roman519@gmail.com>
2019-08-29 22:32:39 -0600

    fileReader was modified to return a list of words without any punctuation

--- wordCount.py
+++ wordCount.py
@@ -1,12 +1,14 @@
-
+import string 
 #Reads file and places each word as part of a list
 def fileReader(file):
-    text = open(file,"r")
-    words = text.read()
-    seperatedWords = words.split()
-    for x in seperatedWords:
-    	print(x)
-    text.close()
+	text = open(file,"r")
+	words = text.read()
+	table = words.maketrans("!;',.?:\"-","         ")
+	words = words.translate(table)
+	seperatedWords = words.split()
+	for x in seperatedWords:
+		print(x)
+	text.close()
 
 #main method
 if __name__=="__main__":
